ea76544ab16d083d66298b0211ac3f6bb44d70e0
Frederick Guevara <fredguevs@gmail.com>
2025-11-22 19:07:48 -0500

    began series

--- 2D_GameEngine/main.cpp
+++ 2D_GameEngine/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, const char * argv[]) {
 
   game = new Game();
 
-  game->init("Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+  game->init("Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640*4, 480*4, false);
 
   while (game->running()) {
     game->handleEvents();
